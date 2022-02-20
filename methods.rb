bd2cbc3c9e83771fd70499c66f05b313660df8a5
Kenneth Kebaara <kkebaara@yahoo.com>
2022-02-19 18:06:56 -0700

    commit tree 6e532b7a46b1e816c42fe8b725d96ce3a6024b67

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
-# Your code here!
+
+
+# function myFunction(param) {
+#     console.log("Running myFunction");
+#     return param + 1;
+#   }
+
+
+#   def my_method(param)
+#     puts "Running my_method"
+#     param + 1
+#   end
+
+def greet_programmer 
+  puts "Hello, programmer!"
+end
+
+def greet(name)
+  puts "Hello, #{name}!"
+end 
+
+def greet_with_default(name = "programmer")
+  puts "Hello, #{name}!"
+end
+
+def add(num1,num2)
+  num1 + num2
+end
+
+def halve(num)
+  if num.class != Integer
+    return nil 
+  end
+
+  num/2
+end
